bff3f359ef61971ab4c8977b5c6b44db3b4b8c4e
sheikhmuhammadzain <zaingee320@gmail.com>
2024-09-17 22:33:12 +0500

    renewed all app

--- scripts/dropdown.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".dropdown").forEach((dropdown) => {
-    dropdown.addEventListener("click", function () {
-      this.classList.toggle("active");
-      this.querySelector(".ri-arrow-down-s-line").classList.toggle(
-        "rotate"
-      );
-    });
-  });
